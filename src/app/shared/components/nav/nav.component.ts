644f9c0fea34fc8af37773620c1f070cb570afd8
Pedro Bicudo <pedrobicudoa@gmail.com>
2023-01-20 16:46:05 -0300

    feat: add event that updates tab name when url changes

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  currentTab!: string;
+
+  constructor(private location: Location) { }
+
   ngOnInit(): void {
+    this.location.onUrlChange((url, _) => {
+      this.currentTab = url[1].toUpperCase()+url.substring(2);
+    });
   }
 
 }
